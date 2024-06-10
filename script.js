125dbb059daddaf81a80f72a6ad09825a4c5a8b1
euronymous22 <105295090+euronymous22@users.noreply.github.com>
2024-06-10 15:24:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -50,13 +50,12 @@ function displayResult(data) {
     const idadeEmpresa = calcularIdadeEmpresa(data.data_inicio_atividade);
 
     resultDiv.innerHTML = `
-        <h2>Dados do CNPJ</h2>
         <table>
             <tr><td>Razão Social</td><td>${data.razao_social ?? 'Não cadastrado'}</td></tr>
-            <tr><td>Nome Fantasia</td><td>${data.nome_fantasia ?? 'Não cadastrado'}</td></tr>
-            <tr><td>Idade da Empresa</td><td>${idadeEmpresa ?? 'Não cadastrado'}</td></tr>
-            <tr><td>Descrição da Situação Cadastral</td><td>${data.descricao_situacao_cadastral ?? 'Não cadastrado'}</td></tr>
-            <tr><td>Descrição do CNAE Fiscal</td><td>${data.cnae_fiscal_descricao ?? 'Não cadastrado'}</td></tr>
+            <tr><td>Fantasia</td><td>${data.nome_fantasia ?? 'Não cadastrado'}</td></tr>
+            <tr><td>Idade</td><td>${idadeEmpresa ?? 'Não cadastrado'}</td></tr>
+            <tr><td>Status</td><td>${data.descricao_situacao_cadastral ?? 'Não cadastrado'}</td></tr>
+            <tr><td>CNAE</td><td>${data.cnae_fiscal_descricao ?? 'Não cadastrado'}</td></tr>
             <tr><td>Telefone 1</td><td>${formatPhoneNumber(data.ddd_telefone_1) ?? 'Não cadastrado'}</td></tr>
             <tr><td>Telefone 2</td><td>${formatPhoneNumber(data.ddd_telefone_2) ?? 'Não cadastrado'}</td></tr>
             <tr><td>Email</td><td>${data.email ?? 'Não cadastrado'}</td></tr>
@@ -90,13 +89,20 @@ function calcularIdadeEmpresa(dataInicio) {
         }
     }
 
-    return `${anos} anos e ${meses} meses`;
+    if (meses === 0) {
+        return `${anos} anos`;
+    } else if (meses === 1) {
+        return `${anos} anos e 1 mês`;
+    } else {
+        return `${anos} anos e ${meses} meses`;
+    }
 }
 
 function formatPhoneNumber(number) {
     if (!number) return null;
 
-    const cleaned = number.replace(/\D/g, '');
+    // Remove o zero à esquerda, se houver
+    const cleaned = number.replace(/^0+/, '').replace(/\D/g, '');
     const match = cleaned.match(/^(\d{2})(\d{4,5})(\d{4})$/);
 
     if (match) {
