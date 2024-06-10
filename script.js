ecc01ba8efd37712969d0cf9ac271c03649140c4
euronymous22 <105295090+euronymous22@users.noreply.github.com>
2024-06-10 14:01:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,78 @@
-body {
-    font-family: Arial, sans-serif;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    background-color: #f0f0f0;
-    margin: 0;
+document.addEventListener("DOMContentLoaded", function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const cnpj = urlParams.get('cnpj');
+    
+    if (cnpj) {
+        mostrarLoading();
+        document.getElementById('cnpj-input').value = cnpj;
+        buscarCNPJ();
+    }
+
+    if (window.self !== window.top) {
+        ajustarParaIframe();
+    }
+});
+
+function ajustarParaIframe() {
+    document.body.style.fontSize = '12px';
+    const container = document.querySelector('.container');
+    container.style.width = '100%';
+    container.style.height = '100%';
+    container.style.padding = '10px';
+    container.style.boxShadow = 'none';
+    container.style.borderRadius = '0';
 }
 
-.container {
-    text-align: center;
-    background-color: #fff;
-    padding: 20px;
-    border-radius: 8px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    width: 100%;
-    max-width: 430px;
-    height: 100%;
-    max-height: 260px;
-    overflow-y: auto;
-    box-sizing: border-box;
+function mostrarLoading() {
+    document.getElementById('loading').style.display = 'block';
+    document.getElementById('input-container').style.display = 'none';
+    document.getElementById('result').style.display = 'none';
 }
 
-#input-container {
-    margin-bottom: 20px;
+function esconderLoading() {
+    document.getElementById('loading').style.display = 'none';
+    document.getElementById('result').style.display = 'block';
+}
+
+async function buscarCNPJ() {
+    const cnpj = document.getElementById('cnpj-input').value;
+    if (!cnpj) {
+        alert('Por favor, insira um CNPJ válido.');
+        esconderLoading();
+        return;
+    }
+
+    try {
+        const response = await fetch(`https://brasilapi.com.br/api/cnpj/v1/${cnpj}`);
+        if (!response.ok) {
+            throw new Error('Erro ao buscar os dados.');
+        }
+
+        const data = await response.json();
+        displayResult(data);
+    } catch (error) {
+        document.getElementById('result').innerText = 'Erro ao buscar os dados. Verifique o CNPJ e tente novamente.';
+        esconderLoading();
+    }
+}
+
+function displayResult(data) {
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = `
+        <table>
+            <tr><td>Razão Social</td><td>${data.razao_social ?? 'Não cadastrado'}</td></tr>
+            <tr><td>Nome Fantasia</td><td>${data.nome_fantasia ?? 'Não cadastrado'}</td></tr>
+            <tr><td>Idade da Empresa</td><td>${calcularIdadeEmpresa(data.data_inicio_atividade) ?? 'Não cadastrado'}</td></tr>
+            <tr><td>Descrição da Situação Cadastral</td><td>${data.descricao_situacao_cadastral ?? 'Não cadastrado'}</td></tr>
+            <tr><td>Descrição do CNAE Fiscal</td><td>${data.cnae_fiscal_descricao ?? 'Não cadastrado'}</td></tr>
+            <tr><td>Telefone 1</td><td>${formatPhoneNumber(data.ddd_telefone_1) ?? 'Não cadastrado'}</td></tr>
+            <tr><td>Telefone 2</td><td>${formatPhoneNumber(data.ddd_telefone_2) ?? 'Não cadastrado'}</td></tr>
+            <tr><td>Email</td><td>${data.email ?? 'Não cadastrado'}</td></tr>
+            <tr><td>Capital Social</td><td>${formatCurrency(data.capital_social)}</td></tr>
+        </table>
+    `;
+
+    esconderLoading();
 }
 
 input {
